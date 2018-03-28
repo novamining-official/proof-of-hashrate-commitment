4ae3b7eaebdf31f0c3f64e9745eb66c54746c670
araspitzu <a.raspitzu@gmail.com>
2018-03-28 14:46:27 +0200

    add PUT for creating tries

--- src/main/scala/api/TreeApi.scala
+++ src/main/scala/api/TreeApi.scala
@@ -3,14 +3,27 @@ package api
 import akka.http.scaladsl.server.Directives
 import common.JsonSupport
 import db.TreeStore
+import proof.MerkleTree.{Account, Tree}
 import proof.MerkleTree.CHAIN_ID._
 
 trait TreeApi extends JsonSupport with Directives {
 
-  def treeRoute = get {
-    pathPrefix("tree" / Segment) { rootDigest =>
-      path("accounts") {
-        complete(TreeStore.findTree(BITCOIN_CHAIN, rootDigest).map(_.accounts))
+  def treeRoute = pathPrefix("tree") {
+    get {
+      pathPrefix(Segment) { rootDigest =>
+        path("accounts") {
+          complete(TreeStore.findTree(BITCOIN_CHAIN, rootDigest).map(_.accounts))
+        }
+      }
+    } ~ put {
+      pathEnd {
+        entity(as[Seq[Account]]) { accounts =>
+          complete {
+            val tree = Tree.build(BITCOIN_CHAIN, accounts)
+            TreeStore.saveTree(tree)
+            tree.rootDigest
+          }
+        }
       }
     }
   }
